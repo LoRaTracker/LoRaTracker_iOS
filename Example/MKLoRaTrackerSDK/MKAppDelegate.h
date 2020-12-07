5566381c5e5a0e0d08b22e3928dffb1f75963283
chengang <aadyx2007@163.com>
2020-12-07 10:42:52 +0800

    Initial commit

---
+++ Example/MKLoRaTrackerSDK/MKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MKAppDelegate.h
+//  MKLoRaTrackerSDK
+//
+//  Created by aadyx2007@163.com on 12/07/2020.
+//  Copyright (c) 2020 aadyx2007@163.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface MKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/MKLoRaTrackerSDK/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MKLoRaTrackerSDK
+//
+//  Created by aadyx2007@163.com on 12/07/2020.
+//  Copyright (c) 2020 aadyx2007@163.com. All rights reserved.
+//
+
+@import UIKit;
+#import "MKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MKAppDelegate class]));
+    }
+}
